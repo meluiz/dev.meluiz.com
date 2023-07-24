37ead45c71ddd2aa7b8ec08c60a9836da2231b99
meluiz <me.luizfelipe@gmail.com>
2023-07-24 03:25:21 -0300

    chore(globals.css): add background grid styles for dark and light themes fix(layout.tsx): change font outfit variable to display swap to improve font loading feat(layout.tsx): add background grid class to body element for styling purposes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import { Outfit, Schibsted_Grotesk } from 'next/font/google'
 import './globals.css'
 
 const outfit = Outfit({
-  variable: '--font-outfit',
+  display: 'swap',
   subsets: ['latin'],
+  variable: '--font-outfit',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 })
 
@@ -21,7 +22,7 @@ const schibstedGrotesk = Schibsted_Grotesk({
 const Layout: App.Layout = function ({ children }) {
   return (
     <html lang="en" className={`dark ${outfit.variable} ${schibstedGrotesk.variable}`}>
-      <body className="w-full min-h-auto bg-white text-mauve-900 font-sans dark:bg-mauve-1300 dark:text-mauve-100">
+      <body className="w-full min-h-auto bg-white text-mauve-900 font-sans dark:bg-mauve-1300 dark:text-mauve-100 bg-grid">
         {children}
       </body>
     </html>
